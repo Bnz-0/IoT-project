c3b6dedcea50022e2620013a82e8a4cd9ce37fc0
strafoxultrapowa <aspem2015@gmail.com>
2020-08-05 20:20:06 +0200

    scanner partial integration with database

--- scanner/debug-scanner.js
+++ scanner/debug-scanner.js
@@ -4,11 +4,17 @@ const fs = require('fs');
 const DEVICES_FILE = "./devices";
 
 module.exports = new EventEmitter();
-module.exports.start = function () {
+module.exports.start2 = function () {
 	setInterval(()=>{
 		const devices = fs.readFileSync(DEVICES_FILE).toString('utf8').split('\n');
 		if(devices[devices.length-1] === '') devices.pop();
 		if(devices.length > 0)
 			this.emit('discover', devices);
+	}, 3000);
+};
+
+module.exports.start = function () {
+	setInterval(()=>{
+		this.emit('discover', ["disp1","disp2"]);
 	}, 3000);
 };
