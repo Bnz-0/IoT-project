a0d3ad30b1c3d1d536ce3dfff1c42f6a0c586c55
Bnz <matteo.benzi97@gmail.com>
2020-08-06 22:07:21 +0200

    remove start2 form debug-scanner

--- scanner/debug-scanner.js
+++ scanner/debug-scanner.js
@@ -4,17 +4,11 @@ const fs = require('fs');
 const DEVICES_FILE = "./devices";
 
 module.exports = new EventEmitter();
-module.exports.start2 = function () {
+module.exports.start = function () {
 	setInterval(()=>{
 		const devices = fs.readFileSync(DEVICES_FILE).toString('utf8').split('\n');
 		if(devices[devices.length-1] === '') devices.pop();
 		if(devices.length > 0)
 			this.emit('discover', devices);
-	}, 3000);
-};
-
-module.exports.start = function () {
-	setInterval(()=>{
-		this.emit('discover', ["disp1","disp2"]);
 	}, 3000);
 };
