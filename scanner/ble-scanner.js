0a9051aea4e105a2ab3f31e9c140214295f49cd9
Bnz <matteo.benzi97@gmail.com>
2020-08-06 22:02:35 +0200

    remade BLE scanner (now it works)

--- scanner/ble-scanner.js
+++ scanner/ble-scanner.js
@@ -1,72 +1,23 @@
-//throw new Error("Not already implemented!");
+'use strict'
+const EventEmitter = require('events');
+const noble = require('noble');
 
-// https://www.npmjs.com/package/node-ble#step-4b-subscribe-to-a-characteristic
-const {createBluetooth} = require('node-ble');
-const {bluetooth, destroy} = createBluetooth();
+/* uuid of the emitter service */
+const service_uuid = "aa5462a4f1124ce88e15830096af8e12";
 
-const interval = 10_000;
-
-function handleReject(action) {
-	return (err) =>	console.error(`An error occurred while ${action}: (${err.name}) ${err.message}`);
+module.exports = new EventEmitter();
+module.exports.start = function() {
+	noble.on('stateChange', function(state) {
+		if (state === 'poweredOn')
+			noble.startScanning(service_uuid);
+		else
+			noble.stopScanning();
+	});
+	
+	noble.on('discover', (peripheral) => {
+		console.log("discovered", peripheral.address);
+		const serviceData = peripheral.advertisement.serviceData;
+		if (serviceData && serviceData.length) 
+			this.emit('discover', serviceData[0].data.toString());
+	});
 }
-
-bluetooth.defaultAdapter().then(
-	async adapter => {
-		try {
-			if (! await adapter.isDiscovering())
-				await adapter.startDiscovery();
-		}
-		catch (err) {
-			handleReject('check/start discovery')(err);
-			return;
-		}
-
-		async function routine() {
-			let devices;
-			try {
-				devices = await adapter.devices(); //TODO: it doesn't "delete" unreachable devices...
-				console.log('devices',devices);
-			} catch(err) {
-				handleReject('getting the device IDs')(err);
-				return;
-			}
-
-			for(let device_id of devices) adapter.waitDevice(device_id).then(
-				async device => {
-					let gattServer;
-					try {
-						await device.connect();
-						gattServer = await device.gatt();
-					} catch(err) {
-						handleReject('connecting to device '+device_id)(err);
-						return;
-					}
-					const services = await gattServer.services();
-					console.log('services of '+device_id, services);
-
-					for(let service_id of services) {
-						if(service_id === "aa5462a4-f112-4ce8-8e15-830096af8e12" || true) { //TODO: the uuid is different: 00001801-0000-1000-8000-00805f9b34fb
-							try{
-								const service = await gattServer.getPrimaryService(service_id);
-								const characteristics = await service.characteristics();
-								for(let char_id of characteristics){
-									const characteristic = await service.getCharacteristic(char_id);
-									const buffer = await characteristic.readValue(); // (DBusError) Read not permitted
-									console.log('BUFFER:',buffer);
-								}
-							} catch(err) {
-								handleReject('reading the characteristic')(err);
-							}
-						}
-					}
-					device.disconnect();
-				},
-				handleReject('waiting for the device '+device_id)
-			);
-		}
-
-		setInterval(routine, interval);
-	}, handleReject('getting the default adapter')
-);
-
-//TODO export
