dc7442381961448cee0640175f6858c724372892
Bnz <matteo.benzi97@gmail.com>
2020-08-27 16:54:15 +0200

    alert push notification

--- emitter-app/app/src/main/java/com/example/icts_emitter/NotificationService.java
+++ emitter-app/app/src/main/java/com/example/icts_emitter/NotificationService.java
@@ -1,7 +1,13 @@
 package com.example.icts_emitter;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 import android.util.Log;
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,7 +23,26 @@ import static com.example.icts_emitter.SharedPreferencesStore.USER_COLLECTION;
 
 public class NotificationService extends FirebaseMessagingService {
     private static final String TAG = "NotificationService";
+    private final String CHANNEL_ID = "ICTS-emitterChannel";
 
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    getString(R.string.channel_name),
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 
     @Override
     public void onNewToken(String token) {
@@ -28,10 +53,16 @@ public class NotificationService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage){
-        Log.d("MessageReceived", "From: " + remoteMessage.getFrom());
-        // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d("MessageReceived", "Message Notification Body: " + remoteMessage.getNotification().getBody());
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentTitle(getString(R.string.notification_title))
+                    .setContentText(remoteMessage.getNotification().getBody())
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            notificationManager.notify(0, builder.build());
         }
     }
 
